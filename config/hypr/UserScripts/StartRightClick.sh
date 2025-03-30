8002ade7ee560c4e671e0a42012771209868d9fa
taylorstools <116524019+taylorstools@users.noreply.github.com>
2025-03-30 12:46:29 -0700

    Add files via upload

--- config/hypr/UserScripts/StartRightClick.sh
+++ config/hypr/UserScripts/StartRightClick.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 UserScripts="$HOME/.config/hypr/UserScripts"
-choice=$(printf "Run\nTerminal\nTerminal (Root)\nTask Manager\nDisk Management\nPower" | wofi --dmenu --width 10% --normal-window --style="$HOME/.config/wofi/style_alt.css")
+choice=$(printf "Run\nTerminal\nTerminal (Root)\nTask Manager\nDisk Management\nPower" | wofi --dmenu --lines=6 --width=11% --location=top_left --conf="$HOME/.config/wofi/config_alt" --style="$HOME/.config/wofi/style_alt.css")
 case "$choice" in
     "Run") kitty --title CheckRunner "$UserScripts/CheckRunner.sh" ;;
     "Terminal") kitty ;;
