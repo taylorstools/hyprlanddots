8e0e8e71463fbbaefb8c88dc8539c5472de3a852
taylorstools <116524019+taylorstools@users.noreply.github.com>
2025-03-31 19:25:06 -0700

    Add files via upload

--- config/hypr/UserScripts/FuzzyRunner.sh
+++ config/hypr/UserScripts/FuzzyRunner.sh
@@ -7,7 +7,7 @@ config_file="$HOME/.config/hypr/wallust/wallust-hyprland.conf"
 color13=$(grep '^\$color13' "$config_file" | sed -E 's/\$color13 = rgb\((.*)\)/#\1/')
 
 #Use fd to search for files and folders and pipe it to fzf
-selected=$(fd . --hidden -tf -td --case-sensitive / | fzf --layout reverse --info hidden --border --query "/home/taylor/" --color "fg:#ffffff,fg+:$color13,selected-fg:$color13,selected-bg:#000000,bg:#000000,list-bg:#000000,border:$color13")
+selected=$(fd . --hidden -tf -td --case-sensitive / | fzf --layout reverse --info hidden --border --query "/home/taylor/" --color "fg:#ffffff,fg+:$color13,selected-fg:$color13,selected-bg:#000000,bg:#000000,list-bg:#000000,border:$color13" --bind "ctrl-a:execute-silent(echo {q} | xclip -selection clipboard)+change-query()")
 
 # If a selection was made and it's a directory, open it with Thunar, else open with xdg-open
 if [ -n "$selected" ]; then
