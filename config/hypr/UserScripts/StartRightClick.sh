35debff520e9b520312bfc19c317c4c12d19d21f
taylorstools <116524019+taylorstools@users.noreply.github.com>
2025-06-21 13:48:18 -0700

    Add files via upload

--- config/hypr/UserScripts/StartRightClick.sh
+++ config/hypr/UserScripts/StartRightClick.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 UserScripts="$HOME/.config/hypr/UserScripts"
-choice=$(printf "Run\nTerminal\nTerminal (Root)\nTask Manager\nDisk Management\nPower" | wofi --dmenu --lines=6 --width=11% --location=top_left --conf="$HOME/.config/wofi/config_alt" --style="$HOME/.config/wofi/style_alt.css")
+choice=$(printf "Run\nTerminal\nTerminal (Root)\nFile Explorer (Root)\nTask Manager\nDisk Management\nPower" | wofi --dmenu --lines=7 --width=10% --location=bottom_left --sort-order=alphabetical --conf="$HOME/.config/wofi/config_alt" --style="$HOME/.config/wofi/style_alt.css")
 case "$choice" in
     "Run") kitty --title CheckRunner "$UserScripts/CheckRunner.sh" ;;
     "Terminal") kitty ;;
     "Terminal (Root)") sudo -E kitty ;;
-    "Task Manager") flatpak run io.missioncenter.MissionCenter ;;
+    "File Explorer (Root)") sudo -E thunar ;;
+    "Task Manager") missioncenter ;;
     "Disk Management") sudo -E gparted ;;
     "Power") "$UserScripts/wlogout.sh" ;;
 esac
