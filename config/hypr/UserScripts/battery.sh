74460da23317048c76d2adee39570329d8e4cf65
taylorstools <116524019+taylorstools@users.noreply.github.com>
2025-06-25 17:40:00 -0700

    Add files via upload

--- config/hypr/UserScripts/battery.sh
+++ config/hypr/UserScripts/battery.sh
@@ -7,7 +7,7 @@ battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
 #Define the battery icons for each 10% segment
-battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
+battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹" "󰁹")
 
 #Define the charging icon
 charging_icon=""
